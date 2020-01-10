763f6ad633b12f65dc17837e2b27eaf76b6aebcb
jmtes <juliennetesoro@gmail.com>
2020-01-09 18:03:48 -0800

    Init item, UI, and app controllers

---
+++ app.js
@@ -0,0 +1,16 @@
+// Storage controller
+
+// Item controller
+const ItemCtrl = (function () {
+  
+})();
+
+// UI controller
+const UICtrl = (function () {
+  
+})();
+
+// App controller
+const AppCtrl = (function (ItemCtrl, UICtrl) {
+  
+})(ItemCtrl, UICtrl);
